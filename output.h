5f15ac5d5c5f3fb46e6dfc878400db208ab18906
KaoutarMk <149963132+KaoutarMk@users.noreply.github.com>
2024-05-25 22:39:12 -0400

    Update output.h

--- output.h
+++ output.h
@@ -1,10 +1,7 @@
+// output.h
 #ifndef OUTPUT_H
 #define OUTPUT_H
 
-#define MAX_WORD_LENGTH 100
+void print_words(char **words, int wordcount);
 
-void sort_words(char words[][MAX_WORD_LENGTH], int num_words, char *sorttype);
-void print_sorted_words(char words[][MAX_WORD_LENGTH], int num_words);
-void print_usage(char *program_name);
-
-#endif /* OUTPUT_H */
+#endif // OUTPUT_H
