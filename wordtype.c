b9214a0f4b47be45c99ce5bd1b7522673f076784
KaoutarMk <149963132+KaoutarMk@users.noreply.github.com>
2024-05-25 22:39:59 -0400

    Update wordtype.c

--- wordtype.c
+++ wordtype.c
@@ -1,13 +1,27 @@
+// wordtype.c
 #include <ctype.h>
 #include <string.h>
 #include "wordtype.h"
 
-int is_word_char(char c, char *wtype) {
-    if (strcmp(wtype, "ALPHA") == 0) {
-        return isalpha(c);
-    } else if (strcmp(wtype, "ALPHANUM") == 0) {
-        return isalnum(c);
-    } else { // Assume ALL
-        return !isspace(c) && c != ',';
+int is_valid_word(const char *word, const char *wtype) {
+    for (int i = 0; word[i] != '\0'; i++) {
+        if (strcmp(wtype, "ALPHA") == 0 && !isalpha(word[i])) {
+            return 0;
+        }
+        if (strcmp(wtype, "ALPHANUM") == 0 && !isalnum(word[i])) {
+            return 0;
+        }
+        if (strcmp(wtype, "ALL") == 0 && !isalnum(word[i]) && word[i] != '_') {
+            return 0;
+        }
     }
+    return 1;
+}
+
+int compare_asc(const void *a, const void *b) {
+    return strcmp(*(const char **)a, *(const char **)b);
+}
+
+int compare_desc(const void *a, const void *b) {
+    return strcmp(*(const char **)b, *(const char **)a);
 }
