f27df2fa512bdb13a8e0f7c2b73d2c0620670509
KaoutarMk <149963132+KaoutarMk@users.noreply.github.com>
2024-05-26 00:14:03 -0400

    Update output.c

--- output.c
+++ output.c
@@ -3,13 +3,13 @@
 #include "output.h"
 
 void print_words(char **words, int wordcount) {
-    for (int i = 0; i < wordcount; i++) {
-        printf("%s", words[i]);
-        if ((i + 1) % 10 == 0) {
-            printf("\n");
-        } else {
-            printf(" ");
-        }
-    }
-    printf("\n");
+for (int i = 0; i < wordcount; i++) {
+printf("%s", words[i]);
+if ((i + 1) % 10 == 0) {
+printf("\n");
+} else {
+printf(" ");
+}
+}
+printf("\n");
 }
