dcd890af55b294662b92c574a6eec13e3261c458
KaoutarMk <149963132+KaoutarMk@users.noreply.github.com>
2024-05-25 22:38:11 -0400

    Update fileread.h

--- fileread.h
+++ fileread.h
@@ -1,10 +1,9 @@
+// fileread.h
 #ifndef FILEREAD_H
 #define FILEREAD_H
 
-#define MAX_WORDS 1000
-#define MAX_WORD_LENGTH 100
+#define MAX_WORD_LEN 100  // Define the maximum length of a word
 
-int read_words(char *input_file, int n, char *wtype, char words[][MAX_WORD_LENGTH], char *skip_words[], int num_skip_words);
-void parse_skip_words(int argc, char *argv[], char *skip_words[], int *num_skip_words);
+int read_words(const char *filename, char **words, int maxwords, const char *wtype, char **skipwords, int skipcount);
 
-#endif /* FILEREAD_H */
+#endif // FILEREAD_H
