19e6e8096e70be5caf93d2a625463b19208f86e1
KaoutarMk <149963132+KaoutarMk@users.noreply.github.com>
2024-05-25 22:40:15 -0400

    Update wordtype.c

--- wordtype.c
+++ wordtype.c
@@ -1,27 +1,9 @@
-// wordtype.c
-#include <ctype.h>
-#include <string.h>
-#include "wordtype.h"
+// wordtype.h
+#ifndef WORDTYPE_H
+#define WORDTYPE_H
 
-int is_valid_word(const char *word, const char *wtype) {
-    for (int i = 0; word[i] != '\0'; i++) {
-        if (strcmp(wtype, "ALPHA") == 0 && !isalpha(word[i])) {
-            return 0;
-        }
-        if (strcmp(wtype, "ALPHANUM") == 0 && !isalnum(word[i])) {
-            return 0;
-        }
-        if (strcmp(wtype, "ALL") == 0 && !isalnum(word[i]) && word[i] != '_') {
-            return 0;
-        }
-    }
-    return 1;
-}
+int is_valid_word(const char *word, const char *wtype);
+int compare_asc(const void *a, const void *b);
+int compare_desc(const void *a, const void *b);
 
-int compare_asc(const void *a, const void *b) {
-    return strcmp(*(const char **)a, *(const char **)b);
-}
-
-int compare_desc(const void *a, const void *b) {
-    return strcmp(*(const char **)b, *(const char **)a);
-}
+#endif // WORDTYPE_H
